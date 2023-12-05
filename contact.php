7a3bc60c21aed04a3fb267fe181e6309fa8f6a6b
enthernetcode <enthernetcode@gmail.com>
2023-12-04 21:38:12 -0600

    .

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  
+  // Set the recipient email address.
+  $recipient = "enthernetcode@gmail.com";
+  
+  // Set the email subject.
+  $subject = "New contact message from $name";
+  
+  // Build the email content.
+  $email_content = "Name: $name\n";
+  $email_content .= "Email: $email\n\n";
+  $email_content .= "Message:\n$message\n";
+  
+  // Build the email headers.
+  $email_headers = "From: $name <$email>";
+  
+  // Send the email.
+  mail($recipient, $subject, $email_content, $email_headers);
+?>
